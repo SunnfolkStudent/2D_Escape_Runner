ea49e8ec677738245ff976dff47fffd2352a251b
UncoloredPlane3 <fredrik.haaven@gmail.com>
2023-09-29 14:48:04 +0200

    made crouch and sprint variable

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,12 +3,18 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    public bool jumpPressed;
-    public bool jumpReleased;
-    public bool jumpHeld;
-    public bool jumpKey;
+    [Header("Jump")]
+    public bool jumpPressed, jumpReleased, jumpHeld, jumpKey;
 
+    [Header("Crouch")] 
+    public bool crouchPressed, crouchReleased;
+    
+    [Header("movement")]
     public Vector2 moveVector;
+
+    [Header("Sprint")] 
+    public bool sprintHeld, SprintReleased;
+    public float stamina = 3;
 
     private void Update()
     {
@@ -20,5 +26,11 @@ public class InputManager : MonoBehaviour
         jumpPressed = Keyboard.current.spaceKey.wasPressedThisFrame;
         jumpReleased = Keyboard.current.spaceKey.wasReleasedThisFrame;
         jumpHeld = Keyboard.current.spaceKey.isPressed;
+        
+        crouchPressed = Keyboard.current.leftCtrlKey.wasPressedThisFrame;
+        crouchReleased = Keyboard.current.leftCtrlKey.wasReleasedThisFrame;
+        
+        sprintHeld = Keyboard.current.leftShiftKey.isPressed;
+        SprintReleased = Keyboard.current.leftShiftKey.wasReleasedThisFrame;
     }
 }
