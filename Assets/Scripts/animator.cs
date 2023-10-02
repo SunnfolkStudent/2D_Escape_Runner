747099b9747fdd0f48701585ddaca0520d45f192
UncoloredPlane3 <fredrik.haaven@gmail.com>
2023-10-02 10:59:31 +0200

    updated player prefab

--- Assets/Scripts/animator.cs
+++ Assets/Scripts/animator.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class animator : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class animator : MonoBehaviour
     {
-        
-    }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+           
+        }
+
+        private void Update()
+        {
+           
+        }
     }
 }
