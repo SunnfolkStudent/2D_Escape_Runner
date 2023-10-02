831b99d281150acbad48f07d9f8d6c5918dc1496
EOppedal <erlend.oppedal@gmail.com>
2023-10-02 16:09:27 +0200

    Fireball cleanup

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -8,15 +8,16 @@ public class Fireball : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
 
     private void Start()
-    {
+    { 
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        
         var rotation = transform.rotation.eulerAngles.z + 90;
-       var angleInRadians = rotation * Mathf.Deg2Rad;
-       var playerMoveDirection = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians));
-       playerMoveDirection.Normalize();
-       _rigidbody2D.velocity = playerMoveDirection * speed;
-       
-       Invoke(nameof(DestroyFireball), flightTime);
+        var angleInRadians = rotation * Mathf.Deg2Rad;
+        var playerMoveDirection = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians));
+        playerMoveDirection.Normalize();
+        _rigidbody2D.velocity = playerMoveDirection * speed;
+        
+        Invoke(nameof(DestroyFireball), flightTime);
     }
 
     private void DestroyFireball()
