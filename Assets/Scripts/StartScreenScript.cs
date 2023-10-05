08b0763ba9f1c98c55803907407514d5f0132f5c
EOppedal <erlend.oppedal@gmail.com>
2023-10-05 19:39:35 +0200

    Start game with controller

--- Assets/Scripts/StartScreenScript.cs
+++ Assets/Scripts/StartScreenScript.cs
@@ -1,8 +1,30 @@
+using System;
+using Player;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartScreenScript : MonoBehaviour
 {
+    private InputManager _input;
+
+    private void Start()
+    {
+        _input = GetComponent<InputManager>();
+    }
+
+    private void Update()
+    {
+        if (_input.jumpPressed)
+        {
+            Play();
+        }
+
+        if (_input.crouchPressed)
+        {
+            Quit();
+        }
+    }
+
     public void Play()
     {
         SceneManager.LoadScene("Tutorial");
